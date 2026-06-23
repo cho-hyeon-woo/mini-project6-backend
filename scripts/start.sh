aba5a173b976c7a8bb9bed225c8982df1c02a02c
HyeonWoo Cho <robert12154937@gmail.com>
2026-06-23 18:50:10 +0900

    scripts update

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-JAR_FILE=$(ls /home/ec2-user/app/build/libs/*.jar | tail -1)
+
+pkill -f 'java -jar' || true
+
+sudo chmod -R 755 /home/ec2-user/app
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app
+
+JAR_FILE=$(ls /home/ec2-user/app/build/libs/*.jar | grep -v plain | tail -1)
 nohup java -jar $JAR_FILE > /home/ec2-user/app/app.log 2>&1 &
