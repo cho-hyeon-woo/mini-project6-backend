932d2bd11c8b7cbbfd9f23990c73c069bfe3a53e
HyeonWoo Cho <robert12154937@gmail.com>
2026-06-24 14:08:58 +0900

    modify

--- src/main/java/com/aivle/bookapp/config/WebConfig.java
+++ src/main/java/com/aivle/bookapp/config/WebConfig.java
@@ -22,8 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
                     "http://localhost:5173", 
                     "http://127.0.0.1:5173",
                     "http://user94-angular-website.s3-website-ap-southeast-1.amazonaws.com",
-                    "http://user94-ALB-1616828866.ap-southeast-1.elb.amazonaws.com"
-                )
+                    "http://user94-ALB-1616828866.ap-southeast-1.elb.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
